835322c326c4b23f9965fd1775c4b7469a5ca283
Big-julian <167761174+Big-julian@users.noreply.github.com>
2024-08-12 06:31:51 +0100

    Add files via upload

--- demo.js
+++ demo.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const inputElement = document.getElementById('productName');
     const remainingCharsElement = document.getElementById('remainingChars');
-
+    const errorMessage = document.getElementById('errorMessage');
     
     const maxLength = inputElement.maxLength;
 
     inputElement.addEventListener('input', () => {
         const currentLength = inputElement.value.length;
         remainingCharsElement.textContent = maxLength - currentLength;
+
+        if(currentLength == 60) {
+            inputElement.style.borderColor = "red"
+            errorMessage.style.color = "red"
+            errorMessage.innerHTML = "Max exceeded!"
+        }
+
     });
 
 
